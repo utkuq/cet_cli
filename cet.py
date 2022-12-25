e4c58d9cf4e2e7cd15666afd127e7c600b69327b
Utku Karataş <utku.karatas3@gmail.com>
2022-12-25 20:57:50 +0300

    Update cet.py

--- cet.py
+++ cet.py
@@ -93,38 +93,55 @@ if a == b == c == d:
                 Fij[i][j] **= -2                    # calculating Fij values
 
    
-    matrix = copy.deepcopy(Fij)
+    matrix = copy.deepcopy(Fij)                                         # copying Fij list in order to make calculations without changing any Fij value
     result = []
-    sum = 0
+    
     for i in range(len(matrix)):
         row=[]
         for j in range(len(matrix[i])):
             if isinstance(matrix[i][j], (int, float)):
             # if matrix[i][j] is int or float:
-                element = matrix[i][j] * productionNumber[j]
-                row.append(element)
+                element = matrix[i][j] * productionNumber[j]            # this code calculates Fij * Pi
+                row.append(element)                                     # adding the result to our result list
                 #print('if worked')
-                sum += element
+                
             else:
-                row.append(matrix[i][j])
+                row.append(matrix[i][j])                                # this code runs if code finds any '-' while doing calculation
                 #print('else worked')
         result.append(row)
     
     print(result)
-    print(f'sum issss: {sum}')
     
-    sumPiFij = 0
-    for i in range(len(result)):
-        print('zzzzz')
-    
-    print(f'sum is {sumPiFij}')
-   
-
                            
     for i in range(len(minutesList)):
         tableMinutesList = minutesList[i]           # creating a new list only for creating table
         tableFijList = Fij[i]                       # creating a new list only for creating table
-        tablePiFijList = result[i]                   # creating a new list only for creating table
+        tablePiFijList = result[i]                 # creating a new list only for creating table
+          
+        for k in range(len(result)):
+            sum = 0
+            for j in range(len(result[k])):
+                if isinstance(result[k][j], (int,float)):
+                    sum += result[k][j]                         # calculating the sum of Pi*Fij values
+                else:
+                    sum += 0
+    
+        divideList = []
+        for u in range(len(result)):
+            row = []
+            for j in range(len(result[u])):
+                if isinstance(result[u][j], (int, float)):
+                    element = result[u][j] / sum
+                    row.append(element)
+                else:
+                    row.append(result[u][j])
+            divideList.append(row)
+          
+        tableDivideList = divideList[i]      
+            
         print(f'For zone {zoneIDs[i]} \n')
-        print(tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi (3*Np - 500)': productionNumber, 'Ai (3*Ne + 75*Af + 400)': attractionNumber, 'C [minutes]': tableMinutesList, 'Fij (C^-2)': tableFijList, 'Pi * Fij': tablePiFijList}, headers='keys'))
+        print(tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi (3*Np - 500)': productionNumber, 'Ai (3*Ne + 75*Af + 400)': attractionNumber, 'C [minutes]': tableMinutesList, 'Fij (C^-2)': tableFijList, 'Pi * Fij': tablePiFijList, 'Pi*Fij / sum': tableDivideList}, headers='keys'))
+        print('\n')
+        print(f'Sum for Pi*Fij at point {zoneIDs[i]}: {sum}')
+        print(f'Divide list for Pi*Fij/sum at point {zoneIDs[i]}: {divideList}')
         print('\n'*2)
