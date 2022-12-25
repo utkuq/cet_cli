b1cae2595ccf3e588a822415c3eef4e9ebe467ec
Utku Karataş <utku.karatas3@gmail.com>
2022-12-25 15:13:37 +0300

    Create cet.py
    this one is lighter version of cet_cli.py

---
+++ cet.py
@@ -0,0 +1,79 @@
+import os
+from pick import pick
+from tabulate import tabulate
+from pyfiglet import figlet_format
+
+os.system('cls')
+
+print(figlet_format('C  E   T \n'))
+print(figlet_format('Civil Engineering Tool \n', font='digital'))
+
+#zone IDs
+zoneIDs = [str(x) for x in input('Enter zone IDs with a space.\n').split()]
+a = len(zoneIDs)
+
+os.system("cls")
+
+#population numbers Np
+populationNumber = [int(y) for y in input('Enter population numbers seperating with a space. (Np)\n').split()]
+b = len(populationNumber)
+
+os.system("cls")
+
+#Floor Area Af
+floorArea = [int(z) for z in input('Enter floor areas with a space. (Af) \n').split()]
+c = len(floorArea)
+
+os.system("cls")
+
+#Employment number Ne
+employmentNumber = [int(t) for t in input('Enter employment number with a space. (Ne)\n').split()]
+d = len(employmentNumber)
+
+os.system('cls')
+
+if a == b == c == d:
+    os.system("cls")
+    print('GIVEN VALUES')
+    print('-'*50 +'\n')
+    print(f'Entered zone IDs: \n {zoneIDs} \n ')
+    print(f'Entered population numbers (Np): \n {populationNumber} \n')
+    print(f'Entered floor areas (Af): \n {floorArea} \n')
+    print(f'Entered employment numbers (Ne): \n {employmentNumber} \n')
+    print('\n'+ '-'*50 + '\n')
+    
+    ################################################ CALCULATIONS ##########################################################
+    ############################################ PHASE 1 CALCULATIONS ######################################################
+    
+    #production number Pi
+    productionNumber = [(3*x)-500 for x in populationNumber]
+    
+    #attraction number Ai
+    attractionNumber1 = [(3*x)+400 for x in employmentNumber]
+    attractionNumber2 = [(75*x) for x in floorArea]
+    attractionNumber = [sum(i) for i in zip(attractionNumber1, attractionNumber2)]
+    
+    ############################################ PHASE 2 CALCULATIONS ######################################################
+    print('\n')
+    counter = 0
+    minutesList=[]
+
+    while counter<len(zoneIDs):
+        pointName = zoneIDs[counter]
+        minutes = [int(o) for o in input(f'Please enter the minutes needed to go from point {pointName} to others with a single space. \nKeep in mind that the order of the numbers are important, enter the numbers as same order as the Zone IDs. \n').split()]
+        os.system('cls')
+        if len(minutes) == len(zoneIDs)-1:
+            minutesList.append(minutes)
+            #print(minutesList)
+            counter+=1
+        else:
+            print('Please enter the right amount of numbers!')
+            minutes=[]
+            counter = 0
+    for i in range(len(minutesList)):
+        minutesList[i].insert(i,'-')
+                           
+    for i in range(len(minutesList)):
+        tableList = minutesList[i]
+        print(tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi': productionNumber, 'Ai': attractionNumber, 'C [minutes]': tableList}, headers='keys'))
+        print('\n'*2)
