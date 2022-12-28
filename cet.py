ce2d11a1512a00ed693229e209309d8ae946363c
Utku Karataş <utku.karatas3@gmail.com>
2022-12-28 20:08:22 +0300

    Update cet.py

--- cet.py
+++ cet.py
@@ -5,7 +5,9 @@ from pyfiglet import figlet_format
 import copy
 import platform
 
-operatingSystem = platform.uname().system                # determining type of the system in order to change clear command between OSs
+# determining type of the system in order to change clear command between OSs
+
+operatingSystem = platform.uname().system                
 if operatingSystem == 'Windows':
     clearVariable = 'cls'
 elif operatingSystem == 'Linux':
@@ -53,8 +55,10 @@ if a == b == c == d:
     print(f'Entered employment numbers (Ne): \n {employmentNumber} \n')
     print('\n'+ '-'*50 + '\n')
     
+    ########################################################################################################################
     ################################################ CALCULATIONS ##########################################################
     ############################################ PHASE 1 CALCULATIONS ######################################################
+    ########################################################################################################################
     
     #production number Pi
     productionNumber = [(3*x)-500 for x in populationNumber]
@@ -64,16 +68,22 @@ if a == b == c == d:
     attractionNumber2 = [(75*x) for x in floorArea]
     attractionNumber = [sum(i) for i in zip(attractionNumber1, attractionNumber2)]
     
+    ########################################################################################################################
     ############################################ PHASE 2 CALCULATIONS ######################################################
+    ########################################################################################################################
+    
     print('\n')
+    
+    
+    # getting minutes input from user
     counter = 0
     minutesList=[]
-
     while counter<len(zoneIDs):
         pointName = zoneIDs[counter]
         os.system(clearVariable)
         minutes = [int(o) for o in input(f'Please enter the minutes needed to go from point {pointName} to others with a single space. \nKeep in mind that the order of the numbers are important, enter the numbers as same order as the Zone IDs. \n').split()]
         os.system(clearVariable)
+        
         if len(minutes) == len(zoneIDs)-1:
             minutesList.append(minutes)
             #print(minutesList)
@@ -86,14 +96,17 @@ if a == b == c == d:
         minutesList[i].insert(i,'-')
     
     
+    # calculating Fij
     Fij = copy.deepcopy(minutesList)                # copying minutes from original list to Fij list in order to keep minutesList as it is.    
     for i in range(len(Fij)):
         for j in range(len(Fij[i])):
             if isinstance(Fij[i][j], int or float):          # checking if the selected element has a type of int
-                Fij[i][j] **= -2                    # calculating Fij values
+                Fij[i][j] **= -2
 
    
-    matrix = copy.deepcopy(Fij)                                         # copying Fij list in order to make calculations without changing any Fij value
+   # this code calculates Fij * Pi
+   
+    matrix = copy.deepcopy(Fij)       # copying Fij list in order to make calculations without changing any Fij value
     result = []
     
     for i in range(len(matrix)):
@@ -101,8 +114,9 @@ if a == b == c == d:
         for j in range(len(matrix[i])):
             if isinstance(matrix[i][j], (int, float)):
             # if matrix[i][j] is int or float:
-                element = matrix[i][j] * productionNumber[j]            # this code calculates Fij * Pi
-                row.append(element)                                     # adding the result to our result list
+                element = matrix[i][j] * productionNumber[j]
+                roundedElement = round(element, 3)            
+                row.append(roundedElement)                                     # adding the result to our result list
                 #print('if worked')
                 
             else:
@@ -112,36 +126,73 @@ if a == b == c == d:
     
     print(result)
     
+    # calculating sigma(Pi*Fij)
+    
+    # defining function to calculate sum
+    def sum_lists(lists):
+        sums = []
+        for l in lists:
+            sum = 0
+            for elem in l:
+                if isinstance(elem, (int, float)):  # Check if the element is a number
+                    sum += elem
+                    roundedSum = round(sum, 3)
+            sums.append(roundedSum)
+        return sums
+
+    # using the function
+    sumListRaw = copy.deepcopy(result)
+    sumList = sum_lists(sumListRaw)
+    print(sumList)
+    
+
+    # calculating Pi*Fij / sigma(Pi*Fij)
+    
+
+    
+    
+    myResult = []
+
+    for i in range(len(result)):
+        row = []
+        for j in range(len(result[i])):
+            if result[i][j] == '-':
+                row.append('-')
+            else:
+                division = result[i][j] / sumList[i]
+                roundedDivision = round(division, 2)
+                row.append(roundedDivision)
+        myResult.append(row)
+
+
+    print(myResult) 
+    
+    
+    divideAndSum = sum_lists(myResult)
+    print(divideAndSum)
+    if divideAndSum == 1.0:
+        print('calculations are correct')
+    else:
+        print('calculations are not correct')
+    
+    
                            
     for i in range(len(minutesList)):
-        tableMinutesList = minutesList[i]           # creating a new list only for creating table
-        tableFijList = Fij[i]                       # creating a new list only for creating table
-        tablePiFijList = result[i]                 # creating a new list only for creating table
-          
-        for k in range(len(result)):
-            sum = 0
-            for j in range(len(result[k])):
-                if isinstance(result[k][j], (int,float)):
-                    sum += result[k][j]                         # calculating the sum of Pi*Fij values
-                else:
-                    sum += 0
-    
-        divideList = []
-        for u in range(len(result)):
-            row = []
-            for j in range(len(result[u])):
-                if isinstance(result[u][j], (int, float)):
-                    element = result[u][j] / sum
-                    row.append(element)
-                else:
-                    row.append(result[u][j])
-            divideList.append(row)
-          
-        tableDivideList = divideList[i]      
-            
+        
+        # creating a new list only for creating table
+        tableMinutesList = minutesList[i]
+        tableFijList = Fij[i] 
+        tablePiFijList = result[i]                 
+        tableDivideList = myResult[i]
+           
+        
+        # printing results    
         print(f'For zone {zoneIDs[i]} \n')
-        print(tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi (3*Np - 500)': productionNumber, 'Ai (3*Ne + 75*Af + 400)': attractionNumber, 'C [minutes]': tableMinutesList, 'Fij (C^-2)': tableFijList, 'Pi * Fij': tablePiFijList, 'Pi*Fij / sum': tableDivideList}, headers='keys'))
+        print(tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi (3*Np - 500)': productionNumber, 'Ai (3*Ne + 75*Af + 400)': attractionNumber, 'C [minutes]': tableMinutesList, 'Fij (C^-2)': tableFijList, 'Pi * Fij': tablePiFijList, 'Pi * Fij / sigma(Pi * Fij)': tableDivideList}, headers='keys', numalign='center', stralign='center'))
         print('\n')
-        print(f'Sum for Pi*Fij at point {zoneIDs[i]}: {sum}')
-        print(f'Divide list for Pi*Fij/sum at point {zoneIDs[i]}: {divideList}')
+        print(f'Sum for Pi*Fij at point {zoneIDs[i]}: {sumList[i]}')
+        print(f'Divide list for Pi*Fij/sum at point {zoneIDs[i]}: {myResult[i]}')
         print('\n'*2)
+        
+
+
