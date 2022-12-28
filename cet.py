67f10ec8ac6b4f527c940f543a4e4dbd7230e088
Utku Karataş <utku.karatas3@gmail.com>
2022-12-28 21:44:58 +0300

    Update cet.py

--- cet.py
+++ cet.py
@@ -25,36 +25,28 @@ for i in range(len(zoneIDs)):
     zoneIDs[i] = zoneIDs[i].upper()
 a = len(zoneIDs)
 
-os.system(clearVariable)
+print('\n')
 
 #population numbers Np
 populationNumber = [int(y) for y in input('Enter population numbers seperating with a space. (Np)\n').split()]
 b = len(populationNumber)
 
-os.system(clearVariable)
+print('\n')
 
 #Floor Area Af
 floorArea = [int(z) for z in input('Enter floor areas with a space. (Af) \n').split()]
 c = len(floorArea)
 
-os.system(clearVariable)
+print('\n')
 
 #Employment number Ne
 employmentNumber = [int(t) for t in input('Enter employment number with a space. (Ne)\n').split()]
 d = len(employmentNumber)
 
-os.system(clearVariable)
+print('\n')
 
 if a == b == c == d:
-    os.system(clearVariable)
-    print('GIVEN VALUES')
-    print('-'*50 +'\n')
-    print(f'Entered zone IDs: \n {zoneIDs} \n ')
-    print(f'Entered population numbers (Np): \n {populationNumber} \n')
-    print(f'Entered floor areas (Af): \n {floorArea} \n')
-    print(f'Entered employment numbers (Ne): \n {employmentNumber} \n')
-    print('\n'+ '-'*50 + '\n')
-    
+
     ########################################################################################################################
     ################################################ CALCULATIONS ##########################################################
     ############################################ PHASE 1 CALCULATIONS ######################################################
@@ -71,18 +63,15 @@ if a == b == c == d:
     ########################################################################################################################
     ############################################ PHASE 2 CALCULATIONS ######################################################
     ########################################################################################################################
-    
-    print('\n')
-    
-    
+
     # getting minutes input from user
     counter = 0
     minutesList=[]
     while counter<len(zoneIDs):
         pointName = zoneIDs[counter]
-        os.system(clearVariable)
-        minutes = [int(o) for o in input(f'Please enter the minutes needed to go from point {pointName} to others with a single space. \nKeep in mind that the order of the numbers are important, enter the numbers as same order as the Zone IDs. \n').split()]
-        os.system(clearVariable)
+        
+        minutes = [int(o) for o in input(f'\nPlease enter the minutes needed to go from point {pointName} to others with a single space.\nKeep in mind that the order of the numbers are important, enter the numbers as same order as the Zone IDs.\n').split()]
+        
         
         if len(minutes) == len(zoneIDs)-1:
             minutesList.append(minutes)
@@ -106,25 +95,31 @@ if a == b == c == d:
    
    # this code calculates Fij * Pi
    
-    matrix = copy.deepcopy(Fij)       # copying Fij list in order to make calculations without changing any Fij value
-    result = []
     
-    for i in range(len(matrix)):
-        row=[]
-        for j in range(len(matrix[i])):
-            if isinstance(matrix[i][j], (int, float)):
-            # if matrix[i][j] is int or float:
-                element = matrix[i][j] * productionNumber[j]
-                roundedElement = round(element, 3)            
-                row.append(roundedElement)                                     # adding the result to our result list
-                #print('if worked')
-                
-            else:
-                row.append(matrix[i][j])                                # this code runs if code finds any '-' while doing calculation
-                #print('else worked')
-        result.append(row)
+    myFijList = copy.deepcopy(Fij)       # copying Fij list in order to make calculations without changing any Fij value
     
-    print(result)
+    def multiplyLists(list1, list2):
+        result = []
+        
+        for i in range(len(list1)):
+            row=[]
+            for j in range(len(list1[i])):
+                if isinstance(list1[i][j], (int, float)):
+                # if matrix[i][j] is int or float:
+                    element = list1[i][j] * list2[j]
+                    roundedElement = round(element, 3)            
+                    row.append(roundedElement)                                     # adding the result to our result list
+                    #print('if worked')
+                    
+                else:
+                    row.append(list1[i][j])                                # this code runs if code finds any '-' while doing calculation
+                    #print('else worked')
+            result.append(row)
+    
+        return result
+    
+    result = multiplyLists(myFijList, productionNumber)
+    
     
     # calculating sigma(Pi*Fij)
     
@@ -143,14 +138,9 @@ if a == b == c == d:
     # using the function
     sumListRaw = copy.deepcopy(result)
     sumList = sum_lists(sumListRaw)
-    print(sumList)
     
 
     # calculating Pi*Fij / sigma(Pi*Fij)
-    
-
-    
-    
     myResult = []
 
     for i in range(len(result)):
@@ -163,36 +153,74 @@ if a == b == c == d:
                 roundedDivision = round(division, 2)
                 row.append(roundedDivision)
         myResult.append(row)
-
-
-    print(myResult) 
+ 
     
-    
+    # calculating the sum of the division above
     divideAndSum = sum_lists(myResult)
-    print(divideAndSum)
-    if divideAndSum == 1.0:
-        print('calculations are correct')
+    
+    # rounding all the numbers in order to check our calculations are correct or not
+    roundedDivideAndSum = [int(number) for number in divideAndSum]
+
+    if all(number == 1 for number in roundedDivideAndSum):
+        os.system(clearVariable)
+        print("calculations are correct \n")
     else:
-        print('calculations are not correct')
+        os.system(clearVariable)
+        print("calculations are not correct \n ")
     
     
-                           
-    for i in range(len(minutesList)):
+    # calculating Ai * ((Pi * Fij) / sigma(Pi * Fij))
+    aPF = multiplyLists(myResult, attractionNumber)
+    
+    # calculating the sum of the list above
+    aPFSumList = sum_lists(aPF)
+    
+    title = 'Would you like to save tables as .txt file?'
+    options = ['Yes', 'No']
+
+    option, index = pick(options, title, indicator='=>', default_index=0)
+    if option == 'Yes':
+        tablo = open('phase_2 - tables.txt', 'w')
         
-        # creating a new list only for creating table
-        tableMinutesList = minutesList[i]
-        tableFijList = Fij[i] 
-        tablePiFijList = result[i]                 
-        tableDivideList = myResult[i]
-           
-        
-        # printing results    
-        print(f'For zone {zoneIDs[i]} \n')
-        print(tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi (3*Np - 500)': productionNumber, 'Ai (3*Ne + 75*Af + 400)': attractionNumber, 'C [minutes]': tableMinutesList, 'Fij (C^-2)': tableFijList, 'Pi * Fij': tablePiFijList, 'Pi * Fij / sigma(Pi * Fij)': tableDivideList}, headers='keys', numalign='center', stralign='center'))
-        print('\n')
-        print(f'Sum for Pi*Fij at point {zoneIDs[i]}: {sumList[i]}')
-        print(f'Divide list for Pi*Fij/sum at point {zoneIDs[i]}: {myResult[i]}')
-        print('\n'*2)
+    
+        # printing results                       
+        for i in range(len(minutesList)):
+            
+            # creating a new list only for creating table
+            tableMinutesList = minutesList[i]
+            tableFijList = Fij[i] 
+            tablePiFijList = result[i]                 
+            tableDivideList = myResult[i]
+            tableAPFList = aPF[i]
+            
+            output = tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi (3*Np - 500)': productionNumber, 'Ai (3*Ne + 75*Af + 400)': attractionNumber, 'C [minutes]': tableMinutesList, 'Fij (C^-2)': tableFijList, 'Pi * Fij': tablePiFijList, 'Pi * Fij / sigma(Pi * Fij)': tableDivideList, 'Ai * ((Pi * Fij) / sigma(Pi * Fij))': tableAPFList}, headers='keys', numalign='center', stralign='center', tablefmt='outline')   
+            
+            tablo.write(f'{output} \n \n')
+            tablo.close
+                
+            print(f'Tables for Phase 2 are saved in the file "phase_2 - tables.txt" ')
+    else:
+        # printing results                       
+        for i in range(len(minutesList)):
+            
+            # creating a new list only for creating table
+            tableMinutesList = minutesList[i]
+            tableFijList = Fij[i] 
+            tablePiFijList = result[i]                 
+            tableDivideList = myResult[i]
+            tableAPFList = aPF[i]
+            
+            output = tabulate({'Zone #': zoneIDs, 'Np': populationNumber, 'Af': floorArea, 'Ne': employmentNumber, 'Pi (3*Np - 500)': productionNumber, 'Ai (3*Ne + 75*Af + 400)': attractionNumber, 'C [minutes]': tableMinutesList, 'Fij (C^-2)': tableFijList, 'Pi * Fij': tablePiFijList, 'Pi * Fij / sigma(Pi * Fij)': tableDivideList, 'Ai * ((Pi * Fij) / sigma(Pi * Fij))': tableAPFList}, headers='keys', numalign='center', stralign='center', tablefmt='outline')   
+                
+            print(f'For zone {zoneIDs[i]} \n')
+            print(output)
+            print('\n')
+            print(f'Sum for Pi*Fij at point {zoneIDs[i]}: {sumList[i]}')
+            print(f'Sum for Ai * ((Pi * Fij) / sigma(Pi * Fij)): {aPFSumList[i]}')
+            print('\n'*2)
+
+    
+    
         
 
 
